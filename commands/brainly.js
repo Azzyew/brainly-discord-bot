98698f08d392b5873f80bee98f59fc3a72482730
azzyew <laisadnevescosta@gmail.com>
2021-03-22 22:13:45 -0300

    feat: adding brainly and calc commands

---
+++ commands/brainly.js
@@ -0,0 +1,28 @@
+const { BrainlyAPI, Server } = require('brainly-api');
+const app = require('../util/app.json');
+
+module.exports = {
+    name: "brainly",
+    cooldown: 3,
+    aliases: ["b"],
+    description: app.__("brainly.description"),
+    async execute(message) {
+      const { question } = message.member;
+
+      BrainlyAPI.startWorker({ experimental: true, server: Server.PT }, async brainly => {
+        const resp = await brainly.findQuestion({question});
+      });
+        try {
+          https.get(url, function (res) {
+            if (res.statusCode == "302") {
+              return message.client.commands.get("brainly").execute(message, [res.headers.location]);
+            } else {
+              return message.reply("No content could be found at that url.").catch(console.error);
+            }
+          });
+        } catch (error) {
+          console.error(error);
+          return message.reply(error.message).catch(console.error);
+        }
+    }
+  };
