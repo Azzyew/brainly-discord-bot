17e44dafe1edf98e0094699caa698ee39a4bc607
azzyew <laisadnevescosta@gmail.com>
2021-03-23 16:03:35 -0300

    adding start script

--- commands/fofoca.js
+++ commands/fofoca.js
@@ -2,6 +2,6 @@ module.exports = {
   name: "fofoca",
   description: "fofoca",
   execute(message) {
-    return message.channel.send("@Fabio202 qual a fofoca do dia?");
+    return message.channel.send("<@!Fabio202> qual a fofoca do dia?");
   }
 };
